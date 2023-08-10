6944217a0f1f418984b5355f5fbe729ad9752504
sarthakroy107 <sarthakroy107@gmail.com>
2023-08-11 01:24:01 +0530

    Audio function changed

--- src/components/VideoDownloaer.jsx
+++ src/components/VideoDownloaer.jsx
@@ -137,7 +137,7 @@ const VideoDownloaer = () => {
             const videoURL = URL.createObjectURL(videoBlob);
             const downloadLink = document.createElement('a');
             downloadLink.href = videoURL;
-            const name = 'Konosuba.mp3';
+            const name = videoDetails.title + 'yuten.mp3';
             downloadLink.setAttribute('download', name);
             console.log("Before on change");
             console.log('After on change');
